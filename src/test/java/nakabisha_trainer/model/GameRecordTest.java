eb599436336d9c3ec165e634d9b18c1e405c7b1e
math-taka <mass.11235813@gmail.com>
2026-08-29 15:17:44 +0900

    Update GameRecord tests for Move side

--- src/test/java/nakabisha_trainer/model/GameRecordTest.java
+++ src/test/java/nakabisha_trainer/model/GameRecordTest.java
@@ -12,7 +12,6 @@ class GameRecordTest {
     @Test
     void startsWithNoMoves() {
         GameRecord record = new GameRecord();
-
         assertEquals(List.of(), record.moves());
         assertEquals(Position.initial().board()[0], record.positionAt(0).board()[0]);
     }
@@ -20,21 +19,17 @@ class GameRecordTest {
     @Test
     void addsAndReturnsMoves() {
         GameRecord record = new GameRecord();
-        Move move = new Move(new Square(7, 7), new Square(7, 6), PieceType.FU, false);
-
+        Move move = new Move(Side.SENTE, new Square(7, 7), new Square(7, 6), PieceType.FU, false);
         record.addMove(move);
-
         assertEquals(List.of(move), record.moves());
     }
 
     @Test
     void returnedMovesCannotModifyRecord() {
         GameRecord record = new GameRecord();
-        Move move = new Move(new Square(7, 7), new Square(7, 6), PieceType.FU, false);
+        Move move = new Move(Side.SENTE, new Square(7, 7), new Square(7, 6), PieceType.FU, false);
         record.addMove(move);
-
         List<Move> moves = record.moves();
-
         assertThrows(UnsupportedOperationException.class, () -> moves.clear());
         assertEquals(List.of(move), record.moves());
     }
@@ -42,15 +37,14 @@ class GameRecordTest {
     @Test
     void rejectsNullMove() {
         GameRecord record = new GameRecord();
-
         assertThrows(NullPointerException.class, () -> record.addMove(null));
     }
 
     @Test
     void returnsPositionAtRequestedPly() {
         GameRecord record = new GameRecord();
-        Move firstMove = new Move(new Square(7, 7), new Square(7, 6), PieceType.FU, false);
-        Move secondMove = new Move(new Square(3, 3), new Square(3, 4), PieceType.FU, false);
+        Move firstMove = new Move(Side.SENTE, new Square(7, 7), new Square(7, 6), PieceType.FU, false);
+        Move secondMove = new Move(Side.GOTE, new Square(3, 3), new Square(3, 4), PieceType.FU, false);
         record.addMove(firstMove);
         record.addMove(secondMove);
 
@@ -58,33 +52,26 @@ class GameRecordTest {
         Position afterFirst = record.positionAt(1);
         Position afterSecond = record.positionAt(2);
 
-        assertEquals(new Piece(Side.SENTE, PieceType.FU, false),
-                initial.pieceAt(new Square(7, 7)));
-        assertEquals(new Piece(Side.SENTE, PieceType.FU, false),
-                afterFirst.pieceAt(new Square(7, 6)));
-        assertEquals(new Piece(Side.GOTE, PieceType.FU, false),
-                afterFirst.pieceAt(new Square(3, 3)));
-        assertEquals(new Piece(Side.GOTE, PieceType.FU, false),
-                afterSecond.pieceAt(new Square(3, 4)));
+        assertEquals(new Piece(Side.SENTE, PieceType.FU, false), initial.pieceAt(new Square(7, 7)));
+        assertEquals(new Piece(Side.SENTE, PieceType.FU, false), afterFirst.pieceAt(new Square(7, 6)));
+        assertEquals(new Piece(Side.GOTE, PieceType.FU, false), afterFirst.pieceAt(new Square(3, 3)));
+        assertEquals(new Piece(Side.GOTE, PieceType.FU, false), afterSecond.pieceAt(new Square(3, 4)));
     }
 
     @Test
     void currentPositionIsPositionAfterAllMoves() {
         GameRecord record = new GameRecord();
-        Move move = new Move(new Square(7, 7), new Square(7, 6), PieceType.FU, false);
+        Move move = new Move(Side.SENTE, new Square(7, 7), new Square(7, 6), PieceType.FU, false);
         record.addMove(move);
-
-        assertEquals(record.positionAt(1).board()[new Square(7, 6).index()],
-                record.currentPosition().board()[new Square(7, 6).index()]);
+        assertEquals(record.positionAt(1).board()[new Square(7, 6).index()], record.currentPosition().board()[new Square(7, 6).index()]);
         assertEquals(record.positionAt(1).sideToMove(), record.currentPosition().sideToMove());
     }
 
     @Test
     void rejectsPlyOutsideRange() {
         GameRecord record = new GameRecord();
-        Move move = new Move(new Square(7, 7), new Square(7, 6), PieceType.FU, false);
+        Move move = new Move(Side.SENTE, new Square(7, 7), new Square(7, 6), PieceType.FU, false);
         record.addMove(move);
-
         assertThrows(IllegalArgumentException.class, () -> record.positionAt(-1));
         assertThrows(IllegalArgumentException.class, () -> record.positionAt(2));
     }
