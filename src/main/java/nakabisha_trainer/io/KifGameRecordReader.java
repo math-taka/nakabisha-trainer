275e83c77bec142a8014ca569f8fdc104b63426e
math-taka <mass.11235813@gmail.com>
2026-09-10 19:08:27 +0900

    Resolve 同 moves in KIF GameRecord reader

--- src/main/java/nakabisha_trainer/io/KifGameRecordReader.java
+++ src/main/java/nakabisha_trainer/io/KifGameRecordReader.java
@@ -2,6 +2,7 @@ package nakabisha_trainer.io;
 
 import nakabisha_trainer.model.GameRecord;
 import nakabisha_trainer.model.Move;
+import nakabisha_trainer.model.Square;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -15,10 +16,24 @@ public final class KifGameRecordReader {
     public static GameRecord read(Path path) throws IOException {
         List<String> lines = KifReader.read(path);
         GameRecord record = new GameRecord();
+        Square previousTo = null;
 
         for (String line : lines) {
             Move move = KifMoveConverter.convert(line);
+
+            if (move.to() == null) {
+                move = new Move(
+                        move.side(),
+                        move.from(),
+                        previousTo,
+                        move.pieceType(),
+                        move.promoted(),
+                        move.promotion()
+                );
+            }
+
             record.addMove(move);
+            previousTo = move.to();
         }
 
         return record;
