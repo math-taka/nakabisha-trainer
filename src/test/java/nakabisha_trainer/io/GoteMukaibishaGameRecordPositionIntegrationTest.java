9accd6a6c72c1a2c54545937be175f0e1a60c0e4
math-taka <mass.11235813@gmail.com>
2026-09-11 16:26:34 +0900

    Correct gote mukaibisha integration test expectations

--- src/test/java/nakabisha_trainer/io/GoteMukaibishaGameRecordPositionIntegrationTest.java
+++ src/test/java/nakabisha_trainer/io/GoteMukaibishaGameRecordPositionIntegrationTest.java
@@ -23,9 +23,7 @@ class GoteMukaibishaGameRecordPositionIntegrationTest {
         Position position = record.positionAt(20);
 
         assertEquals(Side.SENTE, position.pieceAt(new Square(2, 8)).side());
-        assertEquals(PieceType.HI, position.pieceAt(new Square(2, 8)).type());
-        assertEquals(Side.SENTE, position.pieceAt(new Square(2, 8)).side());
-        assertEquals(PieceType.HI, position.pieceAt(new Square(2, 8)).type());
+        assertEquals(PieceType.FU, position.pieceAt(new Square(2, 8)).type());
         assertEquals(Side.SENTE, position.pieceAt(new Square(3, 8)).side());
         assertEquals(PieceType.KIN, position.pieceAt(new Square(3, 8)).type());
         assertEquals(Side.SENTE, position.pieceAt(new Square(5, 8)).side());
@@ -37,18 +35,19 @@ class GoteMukaibishaGameRecordPositionIntegrationTest {
 
         assertEquals(Side.GOTE, position.pieceAt(new Square(2, 2)).side());
         assertEquals(PieceType.GIN, position.pieceAt(new Square(2, 2)).type());
-        assertEquals(Side.GOTE, position.pieceAt(new Square(4, 1)).side());
-        assertEquals(PieceType.KIN, position.pieceAt(new Square(4, 1)).type());
+        assertEquals(Side.GOTE, position.pieceAt(new Square(3, 4)).side());
+        assertEquals(PieceType.FU, position.pieceAt(new Square(3, 4)).type());
         assertEquals(Side.GOTE, position.pieceAt(new Square(4, 2)).side());
         assertEquals(PieceType.KIN, position.pieceAt(new Square(4, 2)).type());
-        assertEquals(Side.GOTE, position.pieceAt(new Square(6, 1)).side());
-        assertEquals(PieceType.GIN, position.pieceAt(new Square(6, 1)).type());
-        assertEquals(Side.GOTE, position.pieceAt(new Square(7, 2)).side());
-        assertEquals(PieceType.OU, position.pieceAt(new Square(7, 2)).type());
+        assertEquals(Side.GOTE, position.pieceAt(new Square(4, 4)).side());
+        assertEquals(PieceType.KAKU, position.pieceAt(new Square(4, 4)).type());
+        assertEquals(Side.GOTE, position.pieceAt(new Square(5, 1)).side());
+        assertEquals(PieceType.KIN, position.pieceAt(new Square(5, 1)).type());
         assertEquals(Side.GOTE, position.pieceAt(new Square(6, 2)).side());
         assertEquals(PieceType.GIN, position.pieceAt(new Square(6, 2)).type());
+        assertEquals(Side.GOTE, position.pieceAt(new Square(7, 2)).side());
+        assertEquals(PieceType.OU, position.pieceAt(new Square(7, 2)).type());
 
-        assertEquals(1, position.hand(Side.SENTE).count(PieceType.FU));
         assertEquals(1, position.hand(Side.SENTE).count(PieceType.HI));
         assertEquals(1, position.hand(Side.GOTE).count(PieceType.HI));
         assertEquals(Side.SENTE, position.sideToMove());
